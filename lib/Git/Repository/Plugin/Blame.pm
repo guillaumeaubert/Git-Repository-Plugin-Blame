e1777b75d3326c674200fae05172789cfb9f33e9
Guillaume Aubert <aubertg@cpan.org>
2012-02-03 23:56:08 -0500

    Released version 1.0.1.

--- lib/Git/Repository/Plugin/Blame.pm
+++ lib/Git/Repository/Plugin/Blame.pm
@@ -20,11 +20,11 @@ Git::Repository::Plugin::Blame - Add a blame() method to L<Git::Repository>.
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 
 =head1 SYNOPSIS
