e958729c8fd32fde70a4cf9ca0e8c93e09625b84
Guillaume Aubert <aubertg@cpan.org>
2012-05-24 10:27:51 -0400

    Released version 1.0.3.

--- lib/Git/Repository/Plugin/Blame/Line.pm
+++ lib/Git/Repository/Plugin/Blame/Line.pm
@@ -14,11 +14,11 @@ Git::Repository::Plugin::Blame::Line - Store the git blame information for a lin
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 
 =head1 SYNOPSIS
