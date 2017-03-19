ec56a925127e1132f4614d10db9614d59325c6ac
Guillaume Aubert <aubertg@cpan.org>
2017-03-19 14:51:01 -0700

    Update copyright year.

--- lib/Git/Repository/Plugin/Blame/Cache.pm
+++ lib/Git/Repository/Plugin/Blame/Cache.pm
@@ -270,7 +270,7 @@ C<< <aubertg at cpan.org> >>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2012-2016 Guillaume Aubert.
+Copyright 2012-2017 Guillaume Aubert.
 
 This code is free software; you can redistribute it and/or modify it under the
 same terms as Perl 5 itself.
