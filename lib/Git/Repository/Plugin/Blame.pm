a6ee5bcd0d7a5ce9bbe6fbac1df4699b7e1412dd
Guillaume Aubert <aubertg@cpan.org>
2017-03-22 22:55:33 -0700

    Release version v1.4.0.

--- lib/Git/Repository/Plugin/Blame.pm
+++ lib/Git/Repository/Plugin/Blame.pm
@@ -21,11 +21,11 @@ Git::Repository::Plugin::Blame - Add a blame() method to L<Git::Repository>.
 
 =head1 VERSION
 
-Version 1.3.0
+Version 1.4.0
 
 =cut
 
-our $VERSION = '1.3.0';
+our $VERSION = '1.4.0';
 
 
 =head1 SYNOPSIS
--- lib/Git/Repository/Plugin/Blame/Line.pm
+++ lib/Git/Repository/Plugin/Blame/Line.pm
@@ -14,11 +14,11 @@ Git::Repository::Plugin::Blame::Line - Store the git blame information for a lin
 
 =head1 VERSION
 
-Version 1.3.0
+Version 1.4.0
 
 =cut
 
-our $VERSION = '1.3.0';
+our $VERSION = '1.4.0';
 
 
 =head1 SYNOPSIS
